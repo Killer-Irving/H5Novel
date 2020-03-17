ab169c79ace73ef310660e5ec95794f884778331
prisoner <540133411@qq.com>
2020-03-17 23:52:41 +0800

    项目初始化搭建；

---
+++ src/pages/theme/index.vue
@@ -0,0 +1,19 @@
+<template></template>
+
+
+<script>
+export default {
+  name:'index',
+  data() {
+    return {};
+  },
+  props: {},
+  components: {},
+  mounted() {},
+  created() {},
+  watch: {},
+  methods: {}
+};
+</script>
+<style lang="scss" scoped>
+</style>
